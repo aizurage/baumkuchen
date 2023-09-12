b34585726325c4003c36d8d915029aba0fb60d8c
1310215 <138311743+1310215@users.noreply.github.com>
2023-09-13 02:05:24 +0900

    Update test.second._index.tsx

--- app/routes/test.second._index.tsx
+++ app/routes/test.second._index.tsx
@@ -4,6 +4,10 @@ export default function AdminIndex() {
   return (
     <main>
         <h1 style={{ color: 'blue', fontSize : 50}}>Test Second Page</h1>
+        <Link to="../quiz" className="text-red-600 underline">
+        クイズに進む  
+        </Link>
+        <p></p>
         <Link to="/test" className="text-red-600 underline">
         {'<<<'}- Link to Test First Page
         </Link>
