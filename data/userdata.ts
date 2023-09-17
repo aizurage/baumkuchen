05f8f81d24c5f7a0ba13402e2c08e8287bf417af
1310215 <kenta14819@gmail.com>
2023-09-18 05:13:24 +0900

    クイズ画面の更新とユーザーのデータが入った模擬ファイルを作成しました。相手のユーザーIDを指定すると、クイズを解答できます。

--- data/userdata.ts
+++ data/userdata.ts
@@ -8,5 +8,6 @@ interface User {
 export const userdata: User[] = [
     { id: '1', name: "サリム", prefecture: '東京都'},
     { id: '2', name: "みそけん", prefecture: '千葉県'},
-    { id: '3', name: "エレ", prefecture: '福島県'},
+    { id: '3', name: "佐々木希美", prefecture: '宮城県'},
+    { id: '4', name: "Fumiaki Hayafuji", prefecture: '福島県'},
 ];
