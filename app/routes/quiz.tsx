1986bfd0e60442089fcbcaa08cf2a42a65df3cf9
1310215 <kenta14819@gmail.com>
2023-09-18 05:02:04 +0900

    クイズ画面の更新とユーザーのデータが入った模擬ファイルを作成しました。相手のユーザーIDを指定すると、クイズを解答できます。

--- app/routes/quiz.tsx
+++ app/routes/quiz.tsx
@@ -1,34 +1,79 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from "@remix-run/react";
 import { prefectures } from '../../data/prefectures';
+import { userdata } from '../../data/userdata';
 
 export default function Quiz() {
-    const [selectedPrefecture, setSelectedPrefecture] = useState("");
-    const correctAnswer = "東京都";
-  
-    return (
-      <div>
-        <h1>都道府県クイズ: 正解は？</h1>
-  
-        {/* 都道府県のドロップダウンリスト */}
-        <select 
-          value={selectedPrefecture} 
-          onChange={(e) => setSelectedPrefecture(e.target.value)}
-        >
-          <option value="" disabled>選択してください</option>
-          {prefectures.map(prefecture => (
-            <option key={prefecture} value={prefecture}>
-              {prefecture}
-            </option>
-          ))}
-        </select>
-        <p></p>
-        {/* 回答ボタン */}
-        {selectedPrefecture !== "" && (
-          <Link to={selectedPrefecture === correctAnswer ? "/correct" : "/incorrect"}>
-            ここをクリックして解答確定
-          </Link>
-        )}
-      </div>
-    );
+  const [userId, setUserId] = useState("");
+  const [selectedPrefecture, setSelectedPrefecture] = useState<string | null>("");
+  const [userExists, setUserExists] = useState<boolean>(true);
+
+  const handleUserIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputUserId = e.target.value;
+    setUserId(inputUserId);
+
+    const user = userdata.find(u => u.id === inputUserId);
+    if (user) {
+      setSelectedPrefecture(user.prefecture);
+      setUserExists(true);
+    } else {
+      setSelectedPrefecture(null);
+      setUserExists(false);
+    }
+  }
+
+  return (
+    <div>
+      <h1>都道府県クイズ！！</h1>
+      <h1>相手のユーザーIDを入力してください</h1>
+      <input 
+        type="text"
+        value={userId}
+        onChange={handleUserIdChange}
+        placeholder="ユーザーIDを入力"
+        style={{ width: "100%", maxWidth: "300px",height: "25px" }}
+      />
+    
+      {userExists ? (
+        selectedPrefecture !== "" && <Answer correctAnswer={selectedPrefecture} />
+      ) : (
+        <p>そのユーザIDは存在しません。</p>
+      )}
+
+    </div>
+  )
+}
+
+type AnswerProps = {
+  correctAnswer: string;
+}
+
+export function Answer({ correctAnswer }: AnswerProps) {
+  const [selectedPrefecture, setSelectedPrefecture] = useState("");
+
+  return (
+    <div>
+      <h1>正解は？</h1>
+
+      {/* 都道府県のドロップダウンリスト */}
+      <select 
+        value={selectedPrefecture} 
+        onChange={(e) => setSelectedPrefecture(e.target.value)}
+      >
+        <option value="" disabled>選択してください</option>
+        {prefectures.map(prefecture => (
+          <option key={prefecture} value={prefecture}>
+            {prefecture}
+          </option>
+        ))}
+      </select>
+
+      {/* 回答ボタン */}
+      {selectedPrefecture !== "" && (
+        <Link to={selectedPrefecture === correctAnswer ? "/correct" : "/incorrect"}>
+          ここをクリックして解答確定
+        </Link>
+      )}
+    </div>
+  );
 }
---
+++ data/userdata.ts
@@ -0,0 +1,12 @@
+interface User {
+    id: string;
+    name: string;
+    prefecture: string;
+}
+
+
+export const userdata: User[] = [
+    { id: '1', name: "サリム", prefecture: '東京都'},
+    { id: '2', name: "みそけん", prefecture: '千葉県'},
+    { id: '3', name: "エレ", prefecture: '福島県'},
+];
