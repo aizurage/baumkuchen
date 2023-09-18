9e1d1b012353ee2a4edf1e0f8463cd5b2259e6d8
1310215 <kenta14819@gmail.com>
2023-09-19 07:03:30 +0900

    正解画面の文字を大きくしました。そのとき大きい文字も設定しました。

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {height: {
+      '140': '40rem'
+      },
+      fontSize: {
+        '12xl':'12rem'
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
