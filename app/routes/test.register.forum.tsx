d2781f5436618ccf05aafdff4e381961677a7dc8
Uirochan <haumuchan@gmail.com>
2023-12-25 18:46:58 +0900

    Post to User-Event Interface added

--- app/routes/test.register.forum.tsx
+++ app/routes/test.register.forum.tsx
@@ -5,10 +5,12 @@ import {
   useLoaderData,
 } from "@remix-run/react";
 
+import type { Event } from "@prisma/client";
 import { getPosts } from "~/models/post.server";
 import { getEvents } from "~/models/test.server";
 import './CSS/postNew.css';
 import QRCodePage from './QR';
+import { joinEvent } from "~/models/test.server";
 
 
 
@@ -16,6 +18,10 @@ export const loader = async () => {
   return json({ posts: await getEvents() });
 };
 
+function handleJoinButtonClick(id: Event["id"]){
+      const userid = "1"; //place holder
+      return joinEvent({id, userid})
+}
 
 export default function PostAdmin() {
   const { posts } = useLoaderData<typeof loader>();
@@ -26,12 +32,22 @@ export default function PostAdmin() {
       </h1>
             {posts.map((post) => (
              <div style={{width :335,marginLeft : 'auto', marginRight: 'auto',borderRadius: 10,overflow:'hidden'}}>
-             <p style={{ backgroundColor: '#D7EEFF', height: 50, fontSize: 30, textAlign: 'center',color:'#FFFFEE',}}>{post.title}</p>
+             <p style={{ backgroundColor: '#D7EEFF', height: 50, fontSize: 30, 
+                              textAlign: 'center',color:'#FFFFEE',}}>{post.title}</p>
              <div style={{ backgroundColor: '#EEFFFF',color:'dodgerblue'}}>
                <p style = {{ fontSize: 20, fontWeight:'bold'}}> {post.creatorid}</p>
                <p style = {{margin: 20}}></p>
                <p style = {{margin: 20}}>{post.description}</p>
                <QRCodePage />
+
+               <button
+                  style={{ backgroundColor: 'lightblue', border: 'none', 
+                                padding: '10px 20px', color: 'black', cursor: 'pointer' }}
+                                onClick={() => handleJoinButtonClick(post.id)}
+                >
+              Join
+              </button>
+
              </div>
              <br></br><br></br>
              </div>
