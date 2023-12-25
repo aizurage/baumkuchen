c0b9a6fa5abc488eaf3e08de7675f998da5a3714
みそけん <138311743+1310215@users.noreply.github.com>
2023-12-26 00:06:05 +0900

    create GamePostの重複の削除

--- prisma/migrations/20231225052747_create_post_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "GamePost" (
-    "title" TEXT NOT NULL PRIMARY KEY,
-    "location" TEXT NOT NULL,
-    "time" TEXT NOT NULL,
-    "markdown" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
